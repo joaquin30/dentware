4fc05389220aea4ab8beaef5bf4959f7c31dce19
Fabian Concha Sifuentes <fabian.santiago27@gmail.com>
2025-06-25 11:40:48 -0500

    agregar la compilacion de procedimientos automaticamente

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
-
 from config import Config
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     app.config['UPLOAD_FOLDER'] = config_class.UPLOAD_FOLDER
 
-    # Initialize Flask extensions here
+    # Initialize Flask extensions
     from app.extensions import db
     db.init_app(app)
+
     from app.gen_fake_data import gen_fake_data
     with app.app_context():
         db.create_all()
-        # Descomenta y corre una vez esta funciona
-        #gen_fake_data()
+        # gen_fake_data()  # Uncomment to populate test data
+
+        # 👇 Ejecutar importación de procedimientos
+        from app.importar_procedimientos import importar_procedimientos_desde_json
+        importar_procedimientos_desde_json()
 
     from app.extensions import csrf
     csrf.init_app(app)
@@ -23,7 +25,7 @@ def create_app(config_class=Config):
     from app.extensions import bootstrap
     bootstrap.init_app(app)
 
-    # Register blueprints here
+    # Register blueprints
     from app.sistema import bp as sistema_bp
     app.register_blueprint(sistema_bp)
 
@@ -34,4 +36,3 @@ def create_app(config_class=Config):
     app.register_blueprint(odontograma_bp, url_prefix='/odontograma')
 
     return app
-
